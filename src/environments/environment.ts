65aa15dbea86ee03027e7ba0c99bbb81999debcf
Emil Buraczynski <emil.buraczynski@decsoft.com.pl>
2018-10-15 15:37:55 +0200

    first commit

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,13 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyAqicstq5DZA7GQ9hVbUbLf8T5Babccdr8',
+    authDomain: 'sk-planes-cc6ce.firebaseapp.com',
+    databaseURL: 'https://sk-planes-cc6ce.firebaseio.com',
+    projectId: 'sk-planes-cc6ce',
+    storageBucket: 'sk-planes-cc6ce.appspot.com',
+    messagingSenderId: '831196261899'
+  }
 };
